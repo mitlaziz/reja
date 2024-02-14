c13c76311a49115c282358cfdf9208a00d3621a1
Max <lazizyuldoshev93@gmail.com>
2024-02-14 16:06:45 +0900

    feat: FrontEng javascriptni qoshish

--- app.js
+++ app.js
@@ -45,12 +45,15 @@ app.post("/create-item", (req, res) => {
     console.log("user entered /create-item");
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end('something went wrong');
-        } else {
-            res.end("successfully added");
-        }
+        console.log(data.ops);
+        res.json(data.ops[0]);
+        // bu traditional post
+        // if(err) {
+        //     console.log(err);
+        //     res.end('something went wrong');
+        // } else {
+        //     res.end("successfully added");
+        // }
     });
     //console.log({finished:well});
     // res.json({ test: "success"});
